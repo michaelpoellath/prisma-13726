04a8f200f89e269fb8bcca7a1100eb4071258163
michaelpoellath <24293821+michaelpoellath@users.noreply.github.com>
2023-06-06 14:24:04 +0200

    chore: demo generator prisma#13726

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# prisma-13726
+# Prisma Generator
+
+Sample Generator to investigate DMMF issues related to issue [#13726](https://github.com/prisma/prisma/issues/13726).
+
+
+
+## Install node modules
+
+```
+npm i
+```
+
+## Build and Run
+
+```
+npm run test
+```
+This command will build the generator and execute `npx prisma generate`.
+
+
+## Conclusion
+
+With the given [schema](prisma/schema.prisma), one can clearly see that only doc strings on models and their fields are shown, while types do not expose any docs via the DMMF.
+
+```
+❯ npm run test
+
+> prisma-generator-test@1.0.0 test
+> npm run build  && npx prisma generate
+
+
+> prisma-generator-test@1.0.0 build
+> npx tsc
+
+Prisma schema loaded from prisma/schema.prisma
+prisma:info prisma-generator-test:Registered
+prisma:info 
+
+==== Print Documentation on Models:
+prisma:info MODEL "person" -- docs: "A Person"
+prisma:info FIELD "name" -- docs: "Name of  the Person"
+prisma:info FIELD "id" -- docs: "undefined"
+prisma:info 
+
+==== Print Documentation on types:
+prisma:info TYPE "name" -- docs: "undefined"
+prisma:info FIELD "firstname" -- docs: "undefined"
+prisma:info FIELD "lastname" -- docs: "undefined"
+...
+```
---
+++ src/generator.ts
@@ -0,0 +1,39 @@
+import { generatorHandler, GeneratorOptions } from '@prisma/generator-helper'
+import { logger } from '@prisma/sdk'
+import { GENERATOR_NAME } from './constants'
+
+const { version } = require('../package.json')
+
+generatorHandler({
+  onManifest() {
+    logger.info(`${GENERATOR_NAME}:Registered`)
+    return {
+      version,
+      defaultOutput: '../generated',
+      prettyName: GENERATOR_NAME,
+    }
+  },
+  onGenerate: async (options: GeneratorOptions) => {
+
+    logger.info('\n\n==== Print Documentation on MODELS:')
+    options.dmmf.datamodel.models.map(m => {
+      logger.info(`MODEL "${m.name}" -- docs: "${m.documentation}"`);
+
+      m.fields.map( f => {
+        logger.info(`FIELD "${f.name}" -- docs: "${f.documentation}"`);
+      })
+    })
+   
+    logger.info('\n\n==== Print Documentation on TYPES:')
+    options.dmmf.datamodel.types.map(t => {
+      logger.info(`TYPE "${t.name}" -- docs: "${t.documentation}"`);
+
+      t.fields.map( f => {
+        logger.info(`FIELD "${f.name}" -- docs: "${f.documentation}"`);
+      })
+    })
+
+    logger.info('\n\n DMMF Datamodel:')
+    logger.info(JSON.stringify(options.dmmf.datamodel, null, 2))
+  },
+})
